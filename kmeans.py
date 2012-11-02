21e38bd58e3251ee313086dfa37231a0f4429f36
fridgei <fridgei@jake.(none)>
2012-11-02 13:03:06 -0700

    new kmeans

--- kmeans.py
+++ kmeans.py
@@ -1,4 +1,4 @@
-from itertools import imap, izip
+from itertools import imap, izip, repeat
 from math import sqrt
 from operator import mul, sub
 import random
@@ -19,6 +19,8 @@ def find_clusters(data, centers):
     return [[point[-1] for point in clusters if point[1] == center] for center in centers]
 
 def kmeans(data, k = 3):
+    diff_sqr = lambda x, y: abs(x-y)**2
+    dist = lambda x1, x2: sum(imap(diff_sqr, x1, x2))**2
     centers = []
     past_centers = []
     while len(centers) < k:
@@ -26,20 +28,20 @@ def kmeans(data, k = 3):
         if point not in centers:
             centers.append(point)
     new_centers = []
+    SSE = []
     while set(new_centers) != set(centers):
+        _SSE = 0
         if new_centers:
             past_centers.append(centers)
             centers = new_centers
         clusters = find_clusters(data, centers)
         new_centers = [find_centroid(cluster) for cluster in clusters]
-    return clusters, past_centers, centers
+        SSE.append(sum([sum(imap(dist, cluster, repeat(center))) for center, cluster in izip(new_centers, clusters)]))
+    return clusters, SSE
 
-_, past_centers, final_centers = kmeans(data)
+cls, SSE = kmeans(data)
 import matplotlib.pyplot as plt
-"""
-SSE = []
-for past_center in past_centers:
-    SSE.append(sum([(c1[0] - c2[0])**2 + (c1[1] - c2[1])**2 for c1, c2 in izip(past_center,final_centers)]))
+
 plt.title("SSE for %s iterations of kmeans with k=3"%len(SSE))
 plt.xlabel("Iterations")
 plt.ylabel("SSE")
@@ -59,3 +61,5 @@ plt.xlabel("k")
 plt.ylabel("SSE")
 plt.plot(range(2,7),SSE)
 plt.show()
+"""
+
